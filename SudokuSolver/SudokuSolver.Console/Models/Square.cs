aba98d2b63f4db9c980c6861bfc4e6f5c1aa2d19
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-08-08 12:57:08 +0100

    Added square logic. Tests passing.

--- SudokuSolver/SudokuSolver.Console/Models/Square.cs
+++ SudokuSolver/SudokuSolver.Console/Models/Square.cs
@@ -1,19 +1,43 @@
-﻿namespace SudokuSolver.Console.Models
+﻿using System;
+
+namespace SudokuSolver.Console.Models
 {
     public class Square
     {
-        public bool ValueIsChangeable { get; }
-        public int Value { get; }
+        private const int EmptySquareValue = 0;
+        private const int MinPermittedValue = 1;
+        private const int MaxPermittedValue = 9;
 
-        public Square(bool valueIsChangeable, int value = 0)
+        public bool ValueIsChangeable { get; }
+        public int? Value { get; private set; }
+
+        public Square(bool valueIsChangeable, int value = EmptySquareValue)
         {
+            CheckValue(value);
+
             ValueIsChangeable = valueIsChangeable;
             Value = value;
         }
 
         public bool TryChangeValue(int value)
         {
+            if (ValueIsChangeable)
+            {
+                CheckValue(value);
+                Value = value;
+                return true;
+            }
+
             return false;
+        }
+
+        private static void CheckValue(int value)
+        {
+            if (value != EmptySquareValue && (value < MinPermittedValue || value > MaxPermittedValue))
+            {
+                throw new ArgumentException(
+                    $"{value} is not a permitted value; should be in range: {MinPermittedValue} - {MaxPermittedValue}.");
+            }
         }
     }
 }
